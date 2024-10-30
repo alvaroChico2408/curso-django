9516a65f9f14797377dbecc0c776b0e059bb4ff1
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-10-30 11:08:07 +0100

    [fix] add html to "saludo" view

--- proyecto1/proyecto1/views.py
+++ proyecto1/proyecto1/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 def saludo(request): #Primera vista
     
-    return HttpResponse("Hola mundo")
+    return HttpResponse("<html><body><h1>Hola mundo</h1></body></html>")
 
 def despedida(request): 
     
