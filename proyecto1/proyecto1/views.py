5a2d16198c697f986fc361476fb7bc3d3b910586
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-10-30 11:02:26 +0100

    [feat] "despedida" view creation

--- proyecto1/proyecto1/views.py
+++ proyecto1/proyecto1/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 def saludo(request): #Primera vista
     
     return HttpResponse("Hola mundo")
+
+def despedida(request): 
+    
+    return HttpResponse("Adiós mundo")
