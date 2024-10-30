34d1a050a71c4a2b5e7a06b3ed43ef598fb08dc9
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-10-30 11:14:02 +0100

    [feat] "fecha" view creation

--- proyecto1/proyecto1/views.py
+++ proyecto1/proyecto1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import datetime
 
 def saludo(request): #Primera vista
     
@@ -16,3 +17,19 @@ def saludo(request): #Primera vista
 def despedida(request): 
     
     return HttpResponse("Adiós mundo")
+
+
+def fecha(request):
+    
+    fecha_actual= datetime.datetime.now()
+    
+    documento= """<html>
+    <body>
+    <h1>
+    Fecha y hora actuales %s
+    </h1>
+    </body>
+    </html>""" %fecha_actual
+    
+    return HttpResponse(documento)
+    
